dc1861a7cf81507996f8237210e7643121013e6f
Mats Saaren <mats.saaren@voco.ee>
2025-04-25 11:16:50 +0300

    Created connection to database

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to sequelize application.' });
 });
